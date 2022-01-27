68c054469095d7574c387c157edc90b122c4bfd8
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-27 14:18:29 -0800

    Use dispatch to load user login info when button triggers the dispatch login action.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {useDispatch} from "react-redux"; //modifies values of state
+import {} from '../features/user';
 
 function Login() {
   const dispatch = useDispatch()
   return (
     <div>
-      <button>Login</button>
+      <button 
+        onClick={() => {
+          dispatch(login({ name: "Pedro", age: 20, email: "pedro@gmail.com"}));
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 }
