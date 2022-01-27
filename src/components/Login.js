3a18f5725ce9e7f23399a4cc253b4d90b852a525
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-27 14:26:53 -0800

    Update the login that was passed from user.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useDispatch} from "react-redux"; //modifies values of state
-import {} from '../features/user';
+import { login } from '../features/user';
 
 function Login() {
   const dispatch = useDispatch()
