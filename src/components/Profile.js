eab2ae996a13c5566343374ff28554065e293031
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-27 14:13:47 -0800

    Add comments indicating what the hooks are.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";//allows for access over state
 
 function Profile() {
   const user = useSelector((state) => state.user.value);
