0bbe54419e93f4812cb209f6a81f47914dfca8e1
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-27 14:13:28 -0800

    Allow dispatch functionality to our Login page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {useDispatch} from "react-redux"; //modifies values of state
 
 function Login() {
+  const dispatch = useDispatch()
   return (
     <div>
       <button>Login</button>
